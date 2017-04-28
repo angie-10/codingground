23e1c861a0cc8d4e635abdc167e196b128f2d78f
angie-10 <angel.alpana@gmail.com>
2017-04-28 13:00:57 +0530

    Created by Coding Ground Client

---
+++ New Project-20170428/Address.java
@@ -0,0 +1,17 @@
+public class Address
+{
+    String city;
+    String state;
+    int post_code;
+    
+    Address(String city, String state, int post_code)
+    {
+        this.city=city;
+        this.state=state;
+        this.post_code=post_code;
+        
+        
+    }
+    
+}
+
